4b5173b3689034a9a3c432de8ed396628392aa16
Joakim Nyman <joakim@edgefive.com>
2012-02-19 14:17:08 +0200

    Added Swedish translations of default.inc.php and recaptcha.inc.php. properties.inc.php still in English for now

---
+++ core/components/formit/lexicon/sv/default.inc.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * FormIt
+ *
+ * Copyright 2009-2011 by Shaun McCormick <shaun@modx.com>
+ *
+ * FormIt is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * FormIt is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * FormIt; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package formit
+ */
+/**
+ * Default Lexicon Topic
+ *
+ * @package formit
+ * @subpackage lexicon
+ * @language sv
+ *
+ * Translation into Swedish by Joakim Nyman <joakim@edgefive.com>
+ */
+$_lang['formit.all_group_text'] = 'Alla länder';
+$_lang['formit.contains'] = 'Ditt värde måste innehålla frasen "[[+value]]".';
+$_lang['formit.email_invalid'] = 'Vänligen ange en giltig e-post adress.';
+$_lang['formit.email_invalid_domain'] = 'Din e-post adress har inte ett giltigt domännamn.';
+$_lang['formit.email_no_recipient'] = 'Vänligen specificera mottagare för e-post meddelandet.';
+$_lang['formit.email_not_sent'] = 'Ett fel uppstod när meddelandet skulle skickas.';
+$_lang['formit.email_tpl_nf'] = 'Vänligen ange en e-post mall.';
+$_lang['formit.field_not_empty'] = 'Detta fält måste vara tomt.';
+$_lang['formit.field_required'] = 'Detta fält är obligatoriskt.';
+$_lang['formit.math_incorrect'] = 'Felaktigt svar!';
+$_lang['formit.math_field_nf'] = 'Fältet [[+field]] ej angivet i formuläret.';
+$_lang['formit.max_length'] = 'Detta fält kan inte vara längre än [[+length]] tecken.';
+$_lang['formit.max_value'] = 'Detta fält kan inte vara större än [[+value]].';
+$_lang['formit.min_length'] = 'Detta fält måste vara åtminstone [[+length]] tecken långt.';
+$_lang['formit.min_value'] = 'Detta fält kan inte vara mindre än [[+value]].';
+$_lang['formit.not_date'] = 'Detta fält måste vara ett giltigt datum.';
+$_lang['formit.not_lowercase'] = 'Detta fält får innehålla endast små bokstäver.';
+$_lang['formit.not_number'] = 'Detta fält måste vara en giltig siffra.';
+$_lang['formit.not_uppercase'] = 'Detta fält får innehålla endast stora bokstäver.';
+$_lang['formit.password_dont_match'] = 'Dina lösenord matchar inte.';
+$_lang['formit.password_not_confirmed'] = 'Vänligen bekräfta ditt lösenord.';
+$_lang['formit.prioritized_group_text'] = 'Frekventa besökare';
+$_lang['formit.range_invalid'] = 'Ogiltig intervall specifikation.';
+$_lang['formit.range'] = 'Ditt värde måste vara mellan [[+min]] och [[+max]].';
+$_lang['formit.recaptcha_err_load'] = 'Kunde inte ladda FormItReCaptcha tjänsteklassen.';
+$_lang['formit.spam_blocked'] = 'Ditt formulär blockerades av ett skräppostfilter: ';
+$_lang['formit.spam_marked'] = ' - markerat som skräppost.';
+$_lang['formit.username_taken'] = 'Användarnamnet är upptaget. Vänligen välj ett annat.';
